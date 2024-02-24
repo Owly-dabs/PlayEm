f377f2a0f4b178aaa095f8f293b2a97b0d4b364e
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-02-24 23:45:03 +0800

    Added Usage Page and Usage assigned HID numbers USB_IF hut1_5

---
+++ app/src/main/java/com/example/playem/hid/usagepages/USAGE_DESKTOP_GENERIC.java
@@ -0,0 +1,14 @@
+package com.example.playem.hid.usagepages;
+
+public final class USAGE_DESKTOP_GENERIC {
+    public final byte PAGE = 0x01;
+    public final byte POINTER = 0x01;
+    public final byte MOUSE = 0x02;
+    public final byte JOYSTICK = 0x04;
+    public final byte GAMEPAD = 0x05;
+    public final byte KEYBOARD = 0x06;
+    public final byte KEYPAD = 0x07;
+    public final byte MACONTROLLER = 0x08;
+    //SubItems
+
+}
