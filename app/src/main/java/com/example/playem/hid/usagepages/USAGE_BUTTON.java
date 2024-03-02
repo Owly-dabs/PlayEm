afa696b9bbe8f83656a14f1b64ee889b9c5b4431
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-02 15:42:42 +0800

    USAGE Page changes

--- app/src/main/java/com/example/playem/hid/usagepages/USAGE_BUTTON.java
+++ app/src/main/java/com/example/playem/hid/usagepages/USAGE_BUTTON.java
@@ -3,4 +3,6 @@ package com.example.playem.hid.usagepages;
 public class USAGE_BUTTON {
     public static byte HIDButton(short ButtonNo) {return ButtonNo>255?0:(byte)ButtonNo;}
     public static byte NoPress = 0;
+
+    public static final byte PAGE = 0x09;
 }
--- app/src/main/java/com/example/playem/hid/usagepages/USAGE_DESKTOP_GENERIC.java
+++ app/src/main/java/com/example/playem/hid/usagepages/USAGE_DESKTOP_GENERIC.java
@@ -1,14 +1,28 @@
 package com.example.playem.hid.usagepages;
 
 public final class USAGE_DESKTOP_GENERIC {
-    public final byte PAGE = 0x01;
-    public final byte POINTER = 0x01;
-    public final byte MOUSE = 0x02;
-    public final byte JOYSTICK = 0x04;
-    public final byte GAMEPAD = 0x05;
-    public final byte KEYBOARD = 0x06;
-    public final byte KEYPAD = 0x07;
-    public final byte MACONTROLLER = 0x08;
+    public static final byte PAGE = 0x01;
+    public static final byte POINTER = 0x01;
+    public static final byte MOUSE = 0x02;
+    public static final byte JOYSTICK = 0x04;
+    public static final byte GAMEPAD = 0x05;
+    public static final byte KEYBOARD = 0x06;
+    public static final byte KEYPAD = 0x07;
+    public static final byte MACONTROLLER = 0x08;
+
+    public static final byte X = 0x30;
+    public static final byte Y = 0x31;
+    public static final byte Z = 0x32;
+    public static final byte Rx = 0x33;
+    public static final byte Ry = 0x34;
+    public static final byte Rz = 0x35;
+    public static final byte Slider = 0x36;
+    public static final byte Dial = 0x37;
+    public static final byte Wheel = 0x38;
+    public static final byte HatSwitch = 0x39;
+
+
+
     //SubItems
 
 }
