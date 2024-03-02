bf1add1d45446dde644cbcae51298ac5c9f75ff0
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-02 15:46:28 +0800

    Updated inner classes to reflect static final fields

--- app/src/main/java/com/example/playem/hid/HIDDescriptor.java
+++ app/src/main/java/com/example/playem/hid/HIDDescriptor.java
@@ -5,22 +5,22 @@ import com.example.playem.hid.usagepages.*;
 public class HIDDescriptor {
 
     public static final class DESCRIPTORIDS{
-        public final byte COLLECTION_START = (byte)0xA1; //Java negative hex values work around
-        public final byte COLLECTION_END = (byte)0xC0;
-        public final byte USAGE_PAGE = 0x05;
-        public final byte USAGE = 0x09;
-        public final byte USAGE_MIN = 0x19;
-        public final byte USAGE_MAX = 0x29;
-        public final byte LOGIC_MIN = 0x15; //length of 1 byte
-        public final byte LOGIC_MAX = 0x25; //length of 1 byte
-        public final byte REPORT_COUNT = (byte)0x95;
-        public final byte REPORT_SIZE = 0x75;
-        public final byte INPUT = (byte)0x81;
+        public static final byte COLLECTION_START = (byte)0xA1; //Java negative hex values work around
+        public static final byte COLLECTION_END = (byte)0xC0;
+        public static final byte USAGE_PAGE = 0x05;
+        public static final byte USAGE = 0x09;
+        public static final byte USAGE_MIN = 0x19;
+        public static final byte USAGE_MAX = 0x29;
+        public static final byte LOGIC_MIN = 0x15; //length of 1 byte
+        public static final byte LOGIC_MAX = 0x25; //length of 1 byte
+        public static final byte REPORT_COUNT = (byte)0x95;
+        public static final byte REPORT_SIZE = 0x75;
+        public static final byte INPUT = (byte)0x81;
     }
     public static final class COLLECTIONTYPES{
-        public final byte APPLICATION = 0x01;
-        public final byte LOGICAL = 0x02;
-        public final byte PHYSICAL = 0x00;
+        public static final byte APPLICATION = 0x01;
+        public static final byte LOGICAL = 0x02;
+        public static final byte PHYSICAL = 0x00;
     }
     public static final class USAGEPAGE{
         public static final USAGE_DESKTOP_GENERIC DESKTOP_GENERIC = new USAGE_DESKTOP_GENERIC();
