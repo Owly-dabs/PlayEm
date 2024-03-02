b88f7ae86448e00897bba1bfbec0396c6ca06b87
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-02 15:45:02 +0800

    Added GattResponse.java static function for GattServerCb functionality

--- app/src/main/java/com/example/playem/btmanager/GattResponse.java
+++ app/src/main/java/com/example/playem/btmanager/GattResponse.java
@@ -8,4 +8,14 @@ public class GattResponse {
     public int ReponseStatus;
     public int offset;
     public byte[] data;
+
+    public static byte[] Slice(byte[] data,int offset){
+        if(offset>=data.length){
+            return new byte[]{}; //Empty
+        }
+        int remainder = data.length-offset;
+        byte[] retarr = new byte[remainder];
+        System.arraycopy(data,offset,retarr,0,remainder);
+        return retarr;
+    }
 }
