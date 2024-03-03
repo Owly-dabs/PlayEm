e6507bf3186dbc6e6146b88b59d307d992247bde
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-04 04:39:27 +0800

    fixed constant RRD value for no Report ID behavior, args left for future implementation

--- app/src/main/java/com/example/playem/btmanager/blehandlers/HIDReportRRDReadRequest.java
+++ app/src/main/java/com/example/playem/btmanager/blehandlers/HIDReportRRDReadRequest.java
@@ -3,16 +3,17 @@ package com.example.playem.btmanager.blehandlers;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattServer;
 
 import com.example.playem.btmanager.GattResponse;
-import com.example.playem.btmanager.blehandlers.interfaces.BLECharacteristicsReadRequest;
 import com.example.playem.btmanager.blehandlers.interfaces.BLEDescriptorReadRequest;
 
 public class HIDReportRRDReadRequest implements BLEDescriptorReadRequest {
-    private final byte[] RRD = new byte[]{(byte) 0x02,0x01};
+    public HIDReportRRDReadRequest(byte nIds,byte reportType,byte maxBufferSize){
+        this.RRD = new byte[]{0x00,0x01};//nIds,reportType};//Page 29 of 123
+    }
+    private byte[] RRD; //= new byte[]{(byte) 0x02,0x01};
     @Override
     public Runnable onDescriptorReadRequest(BluetoothGattServer gattServer, BluetoothDevice device, int requestId, int offset, BluetoothGattDescriptor descriptor) {
         return new Runnable() {
@@ -20,7 +21,7 @@ public class HIDReportRRDReadRequest implements BLEDescriptorReadRequest {
             @Override
             public void run() {
                 int o = offset>=RRD.length?0:offset;
-                gattServer.sendResponse(device,requestId ,BluetoothGatt.GATT_SUCCESS,offset, GattResponse.Slice(RRD,offset));
+                gattServer.sendResponse(device,requestId ,BluetoothGatt.GATT_SUCCESS,offset, GattResponse.Slice(RRD,o));
             }
         };
     }
