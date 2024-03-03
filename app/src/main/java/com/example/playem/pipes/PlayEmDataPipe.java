c43f77b5ae937e5433045e5af8c8ef2ab1a96fea
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-04 04:43:46 +0800

    Added UpdateAxes thread safe; Removed ReportID from GetReport; Added NotifyComplete-> Skips writing to GattServer, reduce BLE radio calls Added signalDirty field: Caller ensures synchronicity

--- app/src/main/java/com/example/playem/pipes/PlayEmDataPipe.java
+++ app/src/main/java/com/example/playem/pipes/PlayEmDataPipe.java
@@ -1,15 +1,17 @@
 package com.example.playem.pipes;
 
-import com.example.playem.btmanager.PlayEmBTManager;
+import android.util.Log;
+
+import com.example.playem.btmanager.blehandlers.interfaces.ConcurrentTransferQueue;
 import com.example.playem.hid.interfaces.ChunkType;
 import com.example.playem.hid.interfaces.HIDChunk;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 //Always takes abs vals from calls assumes last input always
 //This class acts as the de-coupler between GUI and BLE Stack
+//TODO Refactor for multi virtual device
 public class PlayEmDataPipe {
     public PlayEmDataPipe(HashMap<Integer,HIDChunk> chunks){
         //Assumes only report id 0 is active and not report id is not sent to host
@@ -30,6 +32,7 @@ public class PlayEmDataPipe {
     }
     // Button No. is 0 indexed
     public void UpdateButtonNumber(int bNo, byte activeTrue){
+        Log.i("PIPE",String.format("Update Button called %d %d",bNo,activeTrue));
         int byteIndex = bNo/8; //Buttons are always start at 0
         activeTrue = activeTrue>0x01?0x01:activeTrue;
         synchronized (this){
@@ -39,6 +42,24 @@ public class PlayEmDataPipe {
             }else{
                 bArray[byteIndex] = (byte) ((bArray[byteIndex]|((1<<bNo)-1))&activeTrue<<bNo);
             }
+            signalDirty = true;
+            //Log.i("PIPE",String.format("Update Button called %d %8s",bNo,Integer.toBinaryString(CurrentTruth.get(0)[byteIndex]).replace(" ","0")));
+        }
+    }
+
+    //Axes No are 0 indexed
+    public void UpdateAxis(int aNo, int value, boolean isNeg){
+        value = isNeg? (value + 0xFFFF): value;
+        synchronized (this) {
+            HIDChunk axesData = AxesData.get(0);
+            if (aNo >= axesData.size / 2) {
+                Log.e("PIPE", "Invalid Axis Number Called");
+                return;
+            }
+            byte[] aArray = CurrentTruth.get(0);
+            aArray[aNo*2+axesData.bIndex] = (byte)(value); //LSB first
+            aArray[aNo*2+axesData.bIndex+1] = (byte)(value>>8);
+            signalDirty = true;
         }
     }
 
@@ -46,17 +67,23 @@ public class PlayEmDataPipe {
         synchronized (this) {
             byte[] newFrame = new byte[tsize];
             System.arraycopy(CurrentTruth.get(0),0,newFrame,0,tsize);
-            dataPipeRef.add(newFrame);
+            dataPipeRef.enqueue(newFrame);
         }
     }
     public byte[] GetReport(){
+        //Log.i("PIPE","GetReport Called");
         synchronized (this){
-            byte[] newFrame = new byte[tsize+1];
-            newFrame[0] = 1;
-            System.arraycopy(CurrentTruth.get(0),0,newFrame,1,tsize);
+            byte[] newFrame = new byte[tsize];//+1];
+            //newFrame[0] = 1;
+            System.arraycopy(CurrentTruth.get(0),0,newFrame,0,tsize);
             return newFrame;
         }
     }
+
+    public void NotifyComplete(){
+        signalDirty = false;
+    }
+    public boolean signalDirty = false;
     public int getTsize(){
         return tsize;
     }
@@ -64,5 +91,5 @@ public class PlayEmDataPipe {
     final List<byte[]> CurrentTruth = new ArrayList<>();
     final List<HIDChunk> ButtonsData = new ArrayList<>();
     final List<HIDChunk> AxesData = new ArrayList<>();
-    public final ConcurrentLinkedQueue<byte[]> dataPipeRef = new ConcurrentLinkedQueue<>();
+    public final ConcurrentTransferQueue dataPipeRef = new ConcurrentTransferQueue();
 }
