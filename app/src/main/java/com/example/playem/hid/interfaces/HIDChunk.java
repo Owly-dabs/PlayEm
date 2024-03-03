deb4e76ade2dfd41bebd1b0073dac6dd7aaa35c0
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-03 22:10:20 +0800

    HIDChunk.java implementation

--- app/src/main/java/com/example/playem/hid/interfaces/HIDChunk.java
+++ app/src/main/java/com/example/playem/hid/interfaces/HIDChunk.java
@@ -1,8 +1,15 @@
 package com.example.playem.hid.interfaces;
 
-public class HIDChunk {
+public class HIDChunk{
     public int bIndex;
     public int size;
+    public int reportID;
     public ChunkType Type;
+    public int GetHash(){
+        return CalHash(Type,reportID);
+    }
 
+    public static int CalHash(ChunkType type,int reportID){
+        return (int)type.b+(reportID<<8);
+    }
 }
