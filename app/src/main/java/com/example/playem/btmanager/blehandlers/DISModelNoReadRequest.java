5ccb273dacecf9482efd75f3a21e24797005fc58
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-03 22:06:38 +0800

    Added characteristics read request implementations

--- app/src/main/java/com/example/playem/btmanager/blehandlers/DISModelNoReadRequest.java
+++ app/src/main/java/com/example/playem/btmanager/blehandlers/DISModelNoReadRequest.java
@@ -9,21 +9,19 @@ import android.bluetooth.BluetoothGattServer;
 import com.example.playem.btmanager.GattResponse;
 import com.example.playem.btmanager.blehandlers.interfaces.BLECharacteristicsReadRequest;
 
-public class HIDInformationCReadRequest implements BLECharacteristicsReadRequest {
-    final byte[] HIDINFO = {0x11,0x01,0x00,0x02}; //LSB b1-b2 1.11 || localization cc || Normally Connected && See pg 77 HID11
+public class DISModelNoReadRequest implements BLECharacteristicsReadRequest {
+    private byte[] DISVendorID = {0x02,0x10,(byte)0xC4,0,1,0,1};
     @Override
     public Runnable onCharacteristicReadRequest(BluetoothGattServer gattServer, BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, int offset) {
         return new Runnable() {
-            @Override
             @SuppressLint("MissingPermission")
+            @Override
             public void run() {
-                int o = offset;
-                if(offset>3){
-                    o =0;
-                    gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,o, new byte[]{0});
-                    return;
+                if(offset>=DISVendorID.length){
+                    gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,0,new byte[]{0});
+                }else{
+                    gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,offset, GattResponse.Slice(DISVendorID,offset));
                 }
-                gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,o, GattResponse.Slice(HIDINFO,o));
             }
         };
     }
--- app/src/main/java/com/example/playem/btmanager/blehandlers/DISSoftIDCReadRequest.java
+++ app/src/main/java/com/example/playem/btmanager/blehandlers/DISSoftIDCReadRequest.java
@@ -9,21 +9,16 @@ import android.bluetooth.BluetoothGattServer;
 import com.example.playem.btmanager.GattResponse;
 import com.example.playem.btmanager.blehandlers.interfaces.BLECharacteristicsReadRequest;
 
-public class HIDInformationCReadRequest implements BLECharacteristicsReadRequest {
-    final byte[] HIDINFO = {0x11,0x01,0x00,0x02}; //LSB b1-b2 1.11 || localization cc || Normally Connected && See pg 77 HID11
+public class DISSoftIDCReadRequest implements BLECharacteristicsReadRequest {
+    private final byte[] SOFT_ID = "0.0.0.1".getBytes();
     @Override
     public Runnable onCharacteristicReadRequest(BluetoothGattServer gattServer, BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, int offset) {
         return new Runnable() {
-            @Override
             @SuppressLint("MissingPermission")
+            @Override
             public void run() {
-                int o = offset;
-                if(offset>3){
-                    o =0;
-                    gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,o, new byte[]{0});
-                    return;
-                }
-                gattServer.sendResponse(device,requestId, BluetoothGatt.GATT_SUCCESS,o, GattResponse.Slice(HIDINFO,o));
+                int o = offset>=SOFT_ID.length?0:offset;
+                gattServer.sendResponse(device,requestId , BluetoothGatt.GATT_SUCCESS,offset, GattResponse.Slice(SOFT_ID,offset));
             }
         };
     }
