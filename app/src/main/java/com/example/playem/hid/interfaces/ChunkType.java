d864f655904dfb506150cc5f227bfcbfa3b45800
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-03 22:09:41 +0800

    ChunkType instantiators

--- app/src/main/java/com/example/playem/hid/interfaces/ChunkType.java
+++ app/src/main/java/com/example/playem/hid/interfaces/ChunkType.java
@@ -1,7 +1,11 @@
 package com.example.playem.hid.interfaces;
 
 public enum ChunkType {
-    AXES,
-    BUTTONS,
-    POINTER
+    AXES((byte)0),
+    BUTTONS((byte)1);
+    public final byte b;
+
+    ChunkType(byte b) {
+        this.b = b;
+    }
 }
