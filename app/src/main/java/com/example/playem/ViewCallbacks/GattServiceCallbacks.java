732762d0aa8da664c5a97d9574e1dfd494a13ee1
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-06 20:53:11 +0800

    Amended for flexibility: onServiceStatusChanged is mandatory for all classes implementing this interface

--- app/src/main/java/com/example/playem/ViewCallbacks/GattServiceCallbacks.java
+++ app/src/main/java/com/example/playem/ViewCallbacks/GattServiceCallbacks.java
@@ -3,17 +3,22 @@ package com.example.playem.ViewCallbacks;
 import android.bluetooth.BluetoothDevice;
 
 import com.example.playem.PlayEmGATTService;
+import com.example.playem.viewmodels.GattServiceState;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public interface GattServiceCallbacks {
-    Runnable onBondedDevicesChange(ConcurrentLinkedQueue<BluetoothDevice> bondedDevices);
-    Runnable onConnectionStateChanged(String d_address, String d_name, PlayEmGATTService.SERVICE_STATES state);
-    Runnable onAdvertisementStateChanged(PlayEmGATTService.SERVICE_STATES state);
-    Runnable onNotifierChanged(PlayEmGATTService.SERVICE_STATES state);
-    Runnable onServicesAddComplete(PlayEmGATTService.SERVICE_STATES state);
-    Runnable onServiceReady(PlayEmGATTService.SERVICE_STATES state);
-    Runnable onGattStatusChanged(PlayEmGATTService.SERVICE_STATES state);
+    default Runnable onBondedDevicesChange(ConcurrentLinkedQueue<BluetoothDevice> bondedDevices){return null;}
+    default Runnable onConnectionStateChanged(String d_address, String d_name, PlayEmGATTService.SERVICE_STATES state){return null;}
+    default Runnable onAdvertisementStateChanged(PlayEmGATTService.SERVICE_STATES state){return null;}
+    default Runnable onNotifierChanged(PlayEmGATTService.SERVICE_STATES state){return null;}
+    //Internal operation callbacks are optional for UI;
+    default Runnable onServicesAddComplete(PlayEmGATTService.SERVICE_STATES state) {return null;}
+    default Runnable onServiceReady(PlayEmGATTService.SERVICE_STATES state){return null;}
+    default Runnable onGattStatusChanged(GattServiceState.SERVICE_STATUS state){return null;};
+
+    Runnable onServiceStatusChanged(GattServiceState newServiceState);
+
 
 
 }
