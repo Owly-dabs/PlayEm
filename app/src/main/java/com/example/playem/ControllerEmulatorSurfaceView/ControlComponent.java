e23d0d87688686671b77e9fcd78b7b40afc2cb46
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-20 03:43:52 +0800

    Updated default positioning functions

--- app/src/main/java/com/example/playem/ControllerEmulatorSurfaceView/ControlComponent.java
+++ app/src/main/java/com/example/playem/ControllerEmulatorSurfaceView/ControlComponent.java
@@ -3,35 +3,55 @@ package com.example.playem.ControllerEmulatorSurfaceView;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
+
+import java.util.Set;
 
 public abstract class ControlComponent {
+    public ControlComponent(){}
     public ControlComponent(int idxX, int idxY,int pixelsPerStep,int pipeId){
-        positionX = idxX;
-        positionY = idxY;
         this.pixelsPerStep = pixelsPerStep;
         pipeID = pipeId;
-        MoveCentre();
+        SetDatums(idxX,idxY);
+    }
+    protected void SetDatums(int idxX, int idxY){
+        positionX = idxX;
+        positionY = idxY;
+        Log.i("DATUM",String.format("%d %d",positionX,positionY));
+        screenCentrePosY = (float)(idxY*pixelsPerStep+(heightSteps*pixelsPerStep/2));
+        screenCentrePosX = (float)(idxX*pixelsPerStep+(widthSteps*pixelsPerStep/2));
         updateDrawSpace();
     }
-    private final int pipeID;
+    public int pipeID;
     protected ControlHandler handler;
     protected Rect drawSpace;
     public int positionX,positionY;
     protected float screenCentrePosX;
     protected float screenCentrePosY;
     public int widthSteps,heightSteps;
-    public final int pixelsPerStep;
+    public int pixelsPerStep;
     public boolean dragging,locked;
     public abstract void Draw(Canvas canvas, Paint bgPaintObject);
-    public void MoveCentre(){
-        screenCentrePosY = (float)(positionX*pixelsPerStep)+(float)(widthSteps*pixelsPerStep)/2.0f;
-        screenCentrePosX = (float)(positionX*pixelsPerStep)+(float)(widthSteps*pixelsPerStep)/2.0f;
+
+    public void ClearDrawSpace(Canvas canvas,Paint bgPaintObject){
+        canvas.drawRect(this.drawSpace,bgPaintObject);
     }
+    public void MoveCentre(int gridW, int gridH){
+        positionX = (gridW-widthSteps)/2;
+        positionY = (gridH-heightSteps)/2;
+        screenCentrePosX = (float)((positionX*pixelsPerStep)+(float)(widthSteps*pixelsPerStep)/2.0f);
+        screenCentrePosY = (float)((positionY*pixelsPerStep)+(float)(heightSteps*pixelsPerStep)/2.0f);
+        updateDrawSpace();
+        Log.i("THUMB",String.format("%d %d %f %f c Pixel point",positionX,positionY,screenCentrePosX,screenCentrePosY));
+    }
+
     private void updateDrawSpace(){
+        if(drawSpace==null)
+            drawSpace = new Rect();
         drawSpace.left = positionX*pixelsPerStep;
         drawSpace.right = drawSpace.left + widthSteps*pixelsPerStep;
         drawSpace.top = positionY*pixelsPerStep;
-        drawSpace.bottom = drawSpace.top + widthSteps*pixelsPerStep;
+        drawSpace.bottom = drawSpace.top + heightSteps*pixelsPerStep;
     }
 
 }
