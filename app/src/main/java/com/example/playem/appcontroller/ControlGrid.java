e59a36137ebedc59aa4db242ab9db9a09ad56f53
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-23 16:49:51 +0800

    Updated to handle clear calls for PadExtra and moved responsibility to push data frame to component

--- app/src/main/java/com/example/playem/appcontroller/ControlGrid.java
+++ app/src/main/java/com/example/playem/appcontroller/ControlGrid.java
@@ -1,56 +1,33 @@
-package com.example.playem.ControllerEmulatorSurfaceView;
+package com.example.playem.appcontroller;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.DisplayCutout;
 import android.view.MotionEvent;
-import android.view.Window;
 import android.view.WindowMetrics;
 import android.widget.Toast;
 
-import androidx.constraintlayout.widget.ConstraintSet;
-
-import com.example.playem.ControllerEmulatorSurfaceView.VirtualControls.VirtualControlTemplates;
-import com.example.playem.ControllerEmulatorSurfaceView.interfaces.Buildable;
-import com.example.playem.ControllerReactiveActivity;
-import com.example.playem.PlayEmGATTService;
+import com.example.playem.appcontroller.interfaces.Buildable;
+import com.example.playem.ControllerActivity;
 import com.example.playem.generics.ConcurrentListBackHashMap;
-import com.example.playem.hid.HIDProfileBuilder;
 import com.example.playem.pipes.PlayEmDataPipe;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.function.Function;
-
-import kotlin.jvm.functions.Function2;
 
 public class ControlGrid {
     public static final int ORIENTATION_POTRAIT=0;
     public static final int ORIENTATION_LANDSCAPE=1;
     private float gridHeight,gridWidth;
-    private final float screenActualWidth,screenActualHeight, density;
     private final int gridPixelLength,gridPixelWidth;
-    private int arrX,arrY;
+    private final float screenActualHeight,screenActualWidth,density;
     private final ControlComponent[][] grid;
-    public int GetGridWidth(){
-        return arrX;
-    }
-    public int GetGridHeight(){
-            return arrY;
-    }
-
-    private ControllerReactiveActivity parentActivity;
+    private final ControllerActivity parentActivity;
+    private float min_unit_len;
     public ControlGrid(Activity activity, float min_physical_unit_len_inches,int orientation){ //
         //WindowMetrics windowMetrics = activity.getWindowManager().getCurrentWindowMetrics();
         WindowMetrics maxWindowMetrics = activity.getWindowManager().getMaximumWindowMetrics();
@@ -65,30 +42,30 @@ public class ControlGrid {
             cutoutLeft = dc.getSafeInsetLeft();
             cutoutBottom = dc.getSafeInsetTop();
         }
-        parentActivity = (ControllerReactiveActivity) activity; //Disgusting downcast TODO
-        density =  activity.getResources().getDisplayMetrics().density*DisplayMetrics.DENSITY_DEFAULT; //approxiamtely screendpi
+        parentActivity = (ControllerActivity) activity; //Disgusting downcast TODO
+        density = activity.getResources().getDisplayMetrics().density * DisplayMetrics.DENSITY_DEFAULT; //approxiamtely screendpi
         gridPixelLength =  maxWindowMetrics.getBounds().height();
         gridPixelWidth = maxWindowMetrics.getBounds().width();
 
-        screenActualWidth =  (gridPixelWidth-cutoutRight-cutoutLeft) / density;
-        screenActualHeight = (gridPixelLength-cutoutTop-cutoutBottom) / density;
+        screenActualWidth = (gridPixelWidth - cutoutRight - cutoutLeft) / density;
+        screenActualHeight = (gridPixelLength - cutoutTop - cutoutBottom) / density;
 
-        gridWidth=orientation==ORIENTATION_POTRAIT? screenActualWidth:screenActualHeight;
-        gridHeight=orientation==ORIENTATION_POTRAIT? screenActualHeight:screenActualWidth;
-
+        gridWidth=orientation==ORIENTATION_POTRAIT? screenActualWidth : screenActualHeight;
+        gridHeight=orientation==ORIENTATION_POTRAIT? screenActualHeight : screenActualWidth;
+        min_unit_len = min_physical_unit_len_inches;
         gridWidth= gridWidth/min_physical_unit_len_inches;
         gridHeight= gridHeight/min_physical_unit_len_inches;
-        /*Log.e("GRID",String.format("%f %f %f %f",cutoutTop,cutoutRight,cutoutBottom,cutoutLeft));
-        Log.e("GRID",String.format("%f %f",screenActualWidth,screenActualHeight));
-        Log.e("GRID",String.format("%f %f",gridWidth,gridHeight));*/
+
         drawCalls = new ConcurrentLinkedQueue<>();
         clearCalls = new ConcurrentLinkedQueue<>();
         grid = new ControlComponent[(int)gridWidth][(int)gridHeight];
+        dataPipe = null;
         //gridHeight = scr
     }
-    public int[] GetScreenInfo(){
+    public int[] GetGridInfo(){
         return new int[]{(int)gridWidth,(int)gridHeight,(int)(gridPixelWidth/gridWidth)};
     }
+
     private final ConcurrentListBackHashMap<Integer,ControlComponent> ActivePointers = new ConcurrentListBackHashMap<>();
     private final ConcurrentListBackHashMap<Integer,Buildable> MasterComponentList = new ConcurrentListBackHashMap<>();
     private final ConcurrentLinkedQueue<ControlComponent> drawCalls;
@@ -96,8 +73,8 @@ public class ControlGrid {
     private PlayEmDataPipe dataPipe;
     private ControlComponent getControlHandler(float x, float y){
 
-        int arrXidx = (int)(x/gridPixelWidth*gridWidth);
-        int arrYidx = (int)(y/gridPixelLength*gridHeight);
+        int arrXidx = (int)Math.min((x/gridPixelWidth*gridWidth),gridWidth);
+        int arrYidx = (int)Math.min((y/gridPixelLength*gridHeight),gridHeight);
         //Log.i("COLL",String.format("%f %f %d %d",x,y,arrXidx,arrYidx));
         //Log.w("CTLGRID",String.format("%d x %d y",arrXidx,arrYidx));
         return grid[arrXidx][arrYidx];
@@ -109,13 +86,11 @@ public class ControlGrid {
         return clearCalls;
     }
     public void SetPipe(PlayEmDataPipe dataPipe){
-        dataPipe = dataPipe;
+        this.dataPipe = dataPipe;
     }
-
     private boolean pipeFramePush = false;
-
     private void handlePack(ControlHandler.ValuePack[] pack){
-        if(dataPipe!=null){
+        /*if(dataPipe!=null){
             pipeFramePush = true;
             switch(pack[0].type){
                 case AXES:
@@ -132,7 +107,9 @@ public class ControlGrid {
                     pipeFramePush = false||pipeFramePush;
                     break;
             }
-        }
+        }else{
+            Log.e("PIPE","Pipe is Null!");
+        }*/
     }
 
     public void newPointer(MotionEvent event,int id,int idx){
@@ -147,6 +124,8 @@ public class ControlGrid {
             ActivePointers.AddorUpdate(id, ch);
             handlePack(ch.handler.onEnter(x,y,id));
             drawCalls.add(ch);
+            ch.clearCount = 0;
+            clearFromBuffer.Remove(ch.pipeID);
         }
     }
 
@@ -160,7 +139,7 @@ public class ControlGrid {
             drawCalls.add(cc);
         }
     }
-
+    public ConcurrentListBackHashMap<Integer,ControlComponent> clearFromBuffer = new ConcurrentListBackHashMap<>();
     private void dropPointer(MotionEvent event,int id, int idx){
         float x = event.getX(idx);
         float y = event.getY(idx);
@@ -170,8 +149,13 @@ public class ControlGrid {
                 handlePack(Objects.requireNonNull(cc.handler.onExit(x,y,id)));
                 drawCalls.add(cc);
                 ActivePointers.Remove(id);
+                cc.clearCount = 5;
+                clearFromBuffer.AddorUpdate(cc.pipeID,cc);
             }
         }
+    }
+    public void PadExtraOnExit(ControlComponent component){
+        handlePack(component.handler.onExit(component.screenCentrePosX,component.screenCentrePosY,-1));
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(building){
@@ -235,26 +219,26 @@ public class ControlGrid {
                         //Log.i("BUILDMODE",String.format("BuildDatum %d %d %f %f %f %f",nDatumX,nDatumY,gridWidth,Math.floor(gridHeight-1),currentX,currentY));
                         RemoveComponent(focusedComponent);
                         focusedBuilding.MoveAndUpdateDrawSpace(nDatumX,nDatumY);
-                        CheckOverlapAndRedraw(focusedComponent);
-                        drawCalls.add(focusedComponent);
+                        CheckOverlap(focusedComponent);
                     }
                 }else{
                     Log.w("BUILDMODE","Focused object not valid!");
                 }
                 break;
             case MotionEvent.ACTION_POINTER_UP:
-
+                    Log.i("BUILDMODE","Multi Touch not consumed for Build");
                 break;
             case MotionEvent.ACTION_UP:
                 if(focusedBuilding!=null){
-                    Log.i("BUILDMODE","Valid Placement");
-
-                    //AcceptEdits();
-                        //BurnIntoGrid(focusedComponent);
+                    if(focusedComponent.colliding){
+                        parentActivity.ShowToastError("Overlap!",Toast.LENGTH_SHORT);
+                    }else{
+                        parentActivity.ShowToastError("Component Placed!",Toast.LENGTH_SHORT);
+                    }
                 }
                 break;
             case MotionEvent.ACTION_CANCEL:
-                Log.w("CONTROL","CANCEL received, Unsupported input handling");
+                Log.w("BUILDMODE","CANCEL received, Unsupported input handling");
         }
         return true;
     }
@@ -300,46 +284,42 @@ public class ControlGrid {
     public boolean building = true;
     private Buildable focusedBuilding;
     private ControlComponent focusedComponent;
-    public boolean AddBuildableComponenet(Buildable buildable,ControlComponent component){
+    public void AddBuildableComponent(Buildable buildable, ControlComponent component,boolean firstBuild){
         this.building = true;
         buildable.NewBuildState(true);
         focusedBuilding = buildable;
-        AddControlComponent(component);
+        AddControlComponent(component,firstBuild);
         MasterComponentList.AddorUpdate(component.pipeID,buildable);
         uniqueComponent++;
         focusedComponent = component;
         parentActivity.HideMenuOptions();
         parentActivity.ShowControlOptions();
-        return true;
+
         //return
     }
-    public boolean AcceptEdits(){
+    public void AcceptEdits(){
         if(focusedBuilding!=null ){
             if(CheckOverlap(focusedComponent)){
                 BurnIntoGrid(focusedComponent);
                 focusedBuilding = null;
                 focusedComponent = null;
-                return true;
+                return;
             }else{
                 parentActivity.ShowToastError("Cannot Overlap!", Toast.LENGTH_SHORT);
             }
         }
         building = true;
-        return false;
     }
-    public boolean AddControlComponent(ControlComponent component){
-        component.MoveCentre((int)gridWidth,(int)gridHeight);
+    public void AddControlComponent(ControlComponent component, boolean firstBuild){
         if(component==null)
-            return false;
-
-        if(!CheckOverlap(component))
-            return false;
-
+            return;
+        if(firstBuild)
+            component.MoveCentre((int)gridWidth,(int)gridHeight);
         component.pipeID = uniqueComponent;
-
-        drawCalls.add(component);
+        if(!CheckOverlap(component))
+            return;
+        //drawCalls.add(component);
         Log.i("GRID",String.format("Added Component %d %d",component.positionX,component.positionY));
-        return true;
     }
 
     private void BurnIntoGrid(ControlComponent component){
@@ -351,7 +331,7 @@ public class ControlGrid {
         }
     }
     private int uniqueComponent=0;
-    public boolean RemoveComponentInFocus(){
+    public void RemoveComponentInFocus(){
         if(focusedBuilding!=null){
             Log.i("REMOVE","Removing");
             clearCalls.add(new Rect(focusedComponent.drawSpace));
@@ -361,41 +341,51 @@ public class ControlGrid {
             focusedBuilding =null;
             //drawCalls.addAll(MasterComponentList.GetValues())
         }
-        return true;
     }
-    public boolean RemoveComponent(ControlComponent component){
+    public void ResizeComponent(int step){
+        if(focusedBuilding!=null){
+            Log.i("RESIZE","Resizing");
+            Buildable b = focusedBuilding;
+            ControlComponent c = focusedComponent;
+            if(c.positionX+c.widthSteps+step<=gridWidth && c.positionY+c.heightSteps+step<gridHeight) {
+                clearCalls.add(new Rect(c.drawSpace));
+                RemoveComponentInFocus();
+                b.Resize(step);
+                b.MoveAndUpdateDrawSpace(c.positionX, c.positionY);
+                AddBuildableComponent(b,c,false);
+            }
+            //MasterComponentList.Remove(focusedComponent.pipeID);
+            //focusedComponent = null;
+            //focusedBuilding =null;
+            //drawCalls.addAll(MasterComponentList.GetValues())
+        }
+    }
+    public void RemoveComponent(ControlComponent component){
         for(int i = component.positionX;i<component.positionX+component.widthSteps;i++){
             for(int j = component.positionY;j<component.positionY+component.heightSteps;j++){
                 if(grid[i][j]==component)
                     grid[i][j] = null;
             }
         }
-        //MasterComponentList.Remove(component.pipeID);
-        return true;
     }
 
     public boolean CheckOverlap(ControlComponent component){
         for(int i = component.positionX;i<component.positionX+component.widthSteps;i++){
             for(int j = component.positionY;j<component.positionY+component.heightSteps;j++){
-                if(grid[i][j]!=null && grid[i][j]!=component)
+                if(grid[i][j]!=null && grid[i][j]!=component){
+                    component.colliding = true;
                     return false;
+                }
             }
         }
-        return true;
-    }
-    public boolean CheckOverlapAndRedraw(ControlComponent component){
-        HashMap<Integer,ControlComponent> overlaps = new HashMap<>();
-        for(int i = component.positionX;i<component.positionX+component.widthSteps;i++){
-            for(int j = component.positionY;j<component.positionY+component.heightSteps;j++){
-                if(grid[i][j]!=null)
-                    if(component!=grid[i][j])
-                        overlaps.put(grid[i][j].pipeID,grid[i][j]);
-            }
-        }
-        drawCalls.addAll(overlaps.values());
+        component.colliding = false;
         return true;
     }
     public List<Buildable> GetComponentList(){
         return this.MasterComponentList.GetValues();
+    }
+
+    public float[] GetScreenInfo() {
+        return new float[]{screenActualWidth,screenActualHeight,min_unit_len};
     }
 }
