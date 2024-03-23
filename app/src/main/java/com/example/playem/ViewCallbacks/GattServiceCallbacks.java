58f924ce9df6ee2ead57c073937015ca5a837f10
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-23 16:51:18 +0800

    Renamed to AppGattService

--- app/src/main/java/com/example/playem/ViewCallbacks/GattServiceCallbacks.java
+++ app/src/main/java/com/example/playem/ViewCallbacks/GattServiceCallbacks.java
@@ -2,18 +2,18 @@ package com.example.playem.ViewCallbacks;
 
 import android.bluetooth.BluetoothDevice;
 
-import com.example.playem.PlayEmGATTService;
+import com.example.playem.AppGattService;
 import com.example.playem.viewmodels.GattServiceState;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public interface GattServiceCallbacks {
     default Runnable onBondedDevicesChange(ConcurrentLinkedQueue<BluetoothDevice> bondedDevices){return null;}
-    default Runnable onConnectionStateChanged(String d_address, String d_name, PlayEmGATTService.SERVICE_STATES state){return null;}
-    default Runnable onAdvertisementStateChanged(PlayEmGATTService.SERVICE_STATES state){return null;}
-    default Runnable onNotifierChanged(PlayEmGATTService.SERVICE_STATES state){return null;}
-    default Runnable onServicesAddComplete(PlayEmGATTService.SERVICE_STATES state) {return null;}
-    default Runnable onServiceReady(PlayEmGATTService.SERVICE_STATES state){return null;}
+    default Runnable onConnectionStateChanged(String d_address, String d_name, AppGattService.SERVICE_STATES state){return null;}
+    default Runnable onAdvertisementStateChanged(AppGattService.SERVICE_STATES state){return null;}
+    default Runnable onNotifierChanged(AppGattService.SERVICE_STATES state){return null;}
+    default Runnable onServicesAddComplete(AppGattService.SERVICE_STATES state) {return null;}
+    default Runnable onServiceReady(AppGattService.SERVICE_STATES state){return null;}
     default Runnable onGattStatusChanged(GattServiceState.SERVICE_STATUS state){return null;};
 
     Runnable onServiceStatusChanged(GattServiceState newServiceState);
