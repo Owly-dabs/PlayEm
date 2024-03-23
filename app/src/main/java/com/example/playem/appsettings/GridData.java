78eafd46cb4fcf984f071dbb461092812b6a63d8
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-23 16:50:39 +0800

    Serializable data types

---
+++ app/src/main/java/com/example/playem/appsettings/GridData.java
@@ -0,0 +1,12 @@
+package com.example.playem.appsettings;
+
+public class GridData {
+    public GridData(float screenWidth,float screenHeight,float min_unit_len){
+        this.screenHeight = screenHeight;
+        this.screenWidth = screenWidth;
+        this.min_unit_len = min_unit_len;
+    }
+    float screenWidth;
+    float screenHeight;
+    float min_unit_len;
+}
