6f44306add10ef7d6563374b19a0b868d161d835
Lucas1188 <64507210+Lucas1188@users.noreply.github.com>
2024-03-28 11:17:55 +0800

    toString methods

--- app/src/main/java/com/example/playem/appsettings/GridData.java
+++ app/src/main/java/com/example/playem/appsettings/GridData.java
@@ -1,12 +1,23 @@
 package com.example.playem.appsettings;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
 public class GridData {
     public GridData(float screenWidth,float screenHeight,float min_unit_len){
         this.screenHeight = screenHeight;
         this.screenWidth = screenWidth;
         this.min_unit_len = min_unit_len;
     }
-    float screenWidth;
-    float screenHeight;
-    float min_unit_len;
+    public float screenWidth;
+    public float screenHeight;
+    public float min_unit_len;
+
+    @NonNull
+    @SuppressLint("DefaultLocale")
+    @Override
+    public String toString(){
+        return String.format("width: %f\nheight: %f\nmin unit: %f",screenWidth,screenHeight,min_unit_len);
+    }
 }
